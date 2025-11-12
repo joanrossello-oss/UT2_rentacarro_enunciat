54223826c28a41d06f5c37cc81e19e7329d5a89f
Joan Rosselló <joanrossello@paucasesnovescifp.cat>
2025-11-12 15:39:58 +0100

    Code refactored using wtforms and validation

--- PostForm.py
+++ PostForm.py
@@ -1,10 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField
+from wtforms import StringField, SubmitField, DateField, SelectField, validators, ValidationError
 from wtforms.validators import DataRequired, Length
 from datetime import date
+from flask import session
+
 
 class PostForm(FlaskForm):
+    # mètode per comprovar si la fetxa es posterior a la fetxa actual
+    def comprovar_fetxa(form, fetxa):
+        if fetxa.data < date.today(): # date.today() -> obtenir la fetxa actual
+            raise ValidationError('La fetxa no pot ser anterior a la actual.')
+        
+    def comprovar_carro_existent(form, camp):
+        fetxa = form.fetxa.data.strftime('%d-%m-%y')
+        carro = form.carro.data
+
+        reserves = session.get('reserves',[])
+        print(reserves)
+
+        for r in reserves:
+            # comprar les dades de les reserves existents amb la peticio de post a fer
+            if r['dia_reserva'] == fetxa and r['carro'] == carro:
+                raise ValidationError('No pots reservar el carro per el dia seleccionat, ja es troba reservat.')
+    
     nom = StringField('nom', validators=[DataRequired(), Length(max=128)])
-    llinatges = StringField('llinatges', validators=[Length(max=128)])
-    fetxa = DateField('Fecha', format='%Y-%m-%d', default=date.today)
+    llinatges = StringField('llinatges', validators=[DataRequired(), Length(max=128)])
+    fetxa = DateField('fetxa', format='%Y-%m-%d', default=date.today, validators=[DataRequired(), comprovar_fetxa])
+    carro = SelectField('carro', choices=['Carro simple', 'carro 2', 'carro 3'], validators=[DataRequired(), comprovar_carro_existent])
     submit = SubmitField('Enviar')
+
+    
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import json
-import datetime
+from datetime import datetime
+from PostForm import PostForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '83d168b4966265e8ae59f10f57a2b535c18668555b1d499b7353c9b9546146d1'
@@ -12,36 +13,48 @@ def llistar():
         llista_carros = json.load(file)
         return render_template('carros.html', llista_carros=llista_carros)
 
-@app.route("/reservar", methods=['GET','POST'])
+@app.route("/reservar", methods=['GET', 'POST'])
 def reservar():
-    if request.method == 'POST':
-        session["nom"] = request.form['nom'].strip()
-        session["llinatges"] = request.form['llinatges'].strip()
-        session["dia_reserva"] = request.form['reserva'].strip()
-        session["carro"] = request.form['carro'].strip()
+    form = PostForm()
+    if form.validate_on_submit():
+        session["nom"] = form.nom.data
+        session["llinatges"] = form.llinatges.data
+        session["dia_reserva"] = form.fetxa.data.strftime('%d-%m-%y') # Verifica el nombre del campo
+        session["carro"] = form.carro.data
 
-        try:
-            data_obj = datetime.datetime.strptime(session["dia_reserva"], '%d-%m-%Y')
-        except ValueError:
-            flash("⚠️ Format de fetxa incorrecte. Empela el format DD-MM-YYYY")
 
+        print(session['dia_reserva'])
         reserves = session.get("reserves", [])
 
-        if comprovar_fetxa(data_obj) and comprovar_carro_existent(session["carro"], data_obj, reserves):
-                nova_reserva = {
-                    "nom": session["nom"],
-                    "llinatges": session["llinatges"],
-                    "dia_reserva": data_obj.strftime('%d-%m-%y'),
-                    "carro": session["carro"]
-                }
-                print(session['dia_reserva'])
-                reserves.append(nova_reserva)
-                session["reserves"] = reserves
-                return redirect(url_for('comprovar_reserves'))
-        else:
-            return redirect(url_for('reservar'))
-        
-    return render_template("carros2.html")
+        nova_reserva = {
+            "nom": session['nom'],
+            "llinatges": session['llinatges'],
+            "dia_reserva": session['dia_reserva'],
+            "carro": session['carro']
+        }
+
+        reserves.append(nova_reserva)
+        session['reserves'] = reserves
+        return redirect(url_for('comprovar_reserves'))
+
+        # if comprovar_fetxa() and comprovar_carro_existent(session["carro"], reserves):
+        #     fetxa_obj = datetime.strptime(session['dia_reserva'], '%Y-%m-%d')
+        #     fetxa_formateada = fetxa_obj.strftime('%d-%m-%y')
+        #     nova_reserva = {
+        #         "nom": session["nom"],
+        #         "llinatges": session["llinatges"],
+        #         "dia_reserva": fetxa_formateada, 
+        #         "carro": session["carro"]
+        #     }
+        #     print(session['dia_reserva'])
+        #     reserves.append(nova_reserva)
+        #     session["reserves"] = reserves
+        #     return redirect(url_for('comprovar_reserves'))
+        # else:
+        #     return redirect(url_for('reservar'))
+
+    return render_template('carros2.html', form=form)
+
 
 @app.route("/comprovar_reserves")
 def comprovar_reserves():
@@ -53,25 +66,6 @@ def comprovar_reserves():
 # def eliminar_sessio():
 #     session.clear()
 #     return redirect(url_for('reservar'))
-
-# mètode per comprovar si la fetxa es posterior a la fetxa actual
-def comprovar_fetxa(data_obj):
-    # obtenir la fetxa actual
-    data = datetime.datetime.now()
-    # convertir el parametre a format datetime per poder comparar amb la fetxa actual
-    if data.date() > data_obj.date():
-        flash("❌ No pots seleccionar una data anterior a avui.")
-        return False
-    return True
-
-# Mètode per comprovar si el carro ja existeix un dia concret
-def comprovar_carro_existent(carro, data_obj, reserves):
-    for element in reserves:
-        dia_reserva = datetime.datetime.strptime(element['dia_reserva'], "%d-%m-%y").date()
-        if element['carro']  == carro and dia_reserva == data_obj.date():
-            flash("❌ No pots reservar el carro per el dia seleccionat, ja es troba reservat.")
-            return False
-    return True
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
